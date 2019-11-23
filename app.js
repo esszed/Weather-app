57edfb1fd2bc20089fd36ee9f9d034fe0964bf94
esszed <56234798+esszed@users.noreply.github.com>
2019-11-23 01:20:52 +0100

    app for heroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path')
 const geocode = require('./utilities/geocode')
 const forecast = require('./utilities/forecast')
 const app = express()
+const port = process.env.PORT || 3000
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '/templates/views'))
 app.use(express.static('public'))
@@ -62,6 +63,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running on port 3000')
 })
